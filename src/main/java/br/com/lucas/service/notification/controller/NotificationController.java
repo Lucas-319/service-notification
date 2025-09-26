6160c0794b5f066b3b7612413df9d33e73076b1b
Lucas Daniel <kingfoxtroi@gmail.com>
2025-09-26 17:44:16 -0300

    feat: add email notification functionality with SMTP configuration

--- src/main/java/br/com/lucas/service/notification/controller/NotificationController.java
+++ src/main/java/br/com/lucas/service/notification/controller/NotificationController.java
@@ -1,6 +1,8 @@
 package br.com.lucas.service.notification.controller;
 
 import br.com.lucas.service.notification.util.NotificationRequest;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/notification")
 public class NotificationController {
 
+    private final JavaMailSender mailSender;
+
+    public NotificationController(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     @PostMapping
     public void sendNotification(@RequestBody NotificationRequest notificationRequest) {
         System.out.println(notificationRequest.message());
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(notificationRequest.email());
+        message.setSubject("Notification");
+        message.setText(notificationRequest.message());
+        mailSender.send(message);
     }
 }
